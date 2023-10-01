14aa54cd1d5fc0ce61db480d6c9630080439927e
bagusnata330 <bagusnata330@gmail.com>
2023-10-01 18:27:32 +0800

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+
+//Import Components
+import NavbarComponent from "./components/NavbarComponent";
+// import FooterComponent from "./components/FooterComponent";
+
+//Import Pages
+import HomePage from "./pages/HomePage";
+import BookingPage from "./pages/BookingPage";
+import TestimonialPage from "./pages/TestimonialPage";
+import FaqPage from "./pages/FaqPage";
+import SyaratKetentuanPage from "./pages/SyaratKetentuanPage";
+
 function App() {
   return (
-    <>
-      <h1>hello nata</h1>
-    </>
+    <div>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/booking" Component={BookingPage} />
+        <Route path="/tertimonial" Component={TestimonialPage} />
+        <Route path="/faq" Component={FaqPage} />
+        <Route path="/syaratdanketentuan" Component={SyaratKetentuanPage} />
+      </Routes>
+      {/* <FooterComponent /> */}
+    </div>
   );
 }
 
