32d28d69aa28cabc6655bf8a4d719979b64d2d04
bagusnata330 <bagusnata330@gmail.com>
2023-10-02 00:40:58 +0800

    add home page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <header className="cover w-100 min-vh-100"></header>
+      <div className="services w-100 min-vh-100 container">
+        <h1 className="text-center my-5">Services</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage
