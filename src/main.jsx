b3e7a28f9b47408e3a429b8941fd287da623a772
bagusnata330 <bagusnata330@gmail.com>
2023-10-02 22:39:59 +0800

    import animate css and aos

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+import { BrowserRouter } from "react-router-dom";
+
 //Import styling
 import "./dist/css/main.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-import { BrowserRouter } from "react-router-dom";
+import "animate.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
