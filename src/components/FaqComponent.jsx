ba3e52fecfffa5d084f7273af0f8934b398484f7
bagusnata330 <bagusnata330@gmail.com>
2023-10-03 15:23:55 +0800

    add faq component

--- src/components/FaqComponent.jsx
+++ src/components/FaqComponent.jsx
@@ -1,7 +1,38 @@
+import { Container, Row, Col, Accordion } from "react-bootstrap";
+import { faq } from "../data/index.js";
+
 const FaqComponent = () => {
   return (
-    <div>FaqComponent</div>
-  )
-}
+    <div className="faq">
+      <Container>
+        <Row>
+          <Col data-aos="zoom-in">
+            <h1 className="text-center fw-bold">
+              Pertanyaan Yang Sering Ditanyakan
+            </h1>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Recusandae, itaque?
+            </p>
+          </Col>
+        </Row>
+        <Row className="d-flex row-cols-lg-2 row-cols-1">
+          {faq.map((data) => {
+            return (
+              <Col key={data.id}>
+                <Accordion className="shadow-sm mb-3">
+                  <Accordion.Item eventKey={data.eventKey}>
+                    <Accordion.Header>{data.title}</Accordion.Header>
+                    <Accordion.Body>{data.desc}</Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default FaqComponent
+export default FaqComponent;
