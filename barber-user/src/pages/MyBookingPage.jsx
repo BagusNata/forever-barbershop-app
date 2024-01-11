3907fe88283ee6d267b9af0e469612bf5f77f0d0
BagusNata <bagusnata330@gmail.com>
2024-01-11 16:48:59 +0800

    implement api and alert for delete booking

--- barber-user/src/pages/MyBookingPage.jsx
+++ barber-user/src/pages/MyBookingPage.jsx
@@ -1,10 +1,11 @@
 import "../assets/css/myBookingPage.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useUserContext } from "../UserContext";
 import { format } from "date-fns";
-
 
 const MyBookingPage = () => {
   let navigate = useNavigate();
@@ -29,7 +30,7 @@ const MyBookingPage = () => {
           setData(bookings);
         }
       } catch (error) {
-        setData([])
+        setData([]);
         console.error("Error fetching data:", error);
       }
     };
@@ -39,6 +40,30 @@ const MyBookingPage = () => {
   function formatDate(dateString) {
     return format(new Date(dateString), "dd MMMM yyyy");
   }
+
+  const handleCancelBooking = async (bookingId) => {
+    try {
+      await fetch(`${import.meta.env.VITE_API_URL}/api/bookings/${bookingId}`, {
+        method: "DELETE",
+        headers: {
+          "x-access-token": userData.accessToken,
+          "Content-Type": "application/json",
+        },
+      });
+
+      // After successful deletion, update the data state to re-render without the deleted booking
+      setData((prevData) =>
+        prevData.filter((booking) => booking.id !== bookingId)
+      );
+
+      // Show a success notification
+      toast.success("Booking canceled successfully!");
+    } catch (error) {
+      console.error("Error deleting booking:", error);
+      // Show an error notification
+      toast.error("Error canceling booking. Please try again later.");
+    }
+  };
 
   return (
     <div className="my-booking w-100 min-vh-100">
@@ -61,73 +86,83 @@ const MyBookingPage = () => {
         </Row>
         {data.length === 0 ? (
           <h5 className="no-booking">
-            You don&apos;t have any ongoing bookings
+            Anda tidak memiliki daftar booking yang sedang berlangsung
           </h5>
         ) : (
           <Row className="row-col-lg-3 row-col-md-3 row-col-sm-1">
-            {data.map((booking) => {
-              if (userData.id === booking.userId) {
-                return (
-                  <Col key={booking.id}>
-                    <Card data-aos="fade-up" className="shadow">
-                      <Card.Body>
-                        <table className="tables">
-                          <thead className="title">Bukti Booking</thead>
-                          <tbody className="information">
-                            <tr>
-                              <th scope="row">Nama</th>
-                              <td>:</td>
-                              <td>{booking.user.username}</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">Tanggal</th>
-                              <td>:</td>
-                              <td>{formatDate(booking.date)}</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">Jam</th>
-                              <td>:</td>
-                              <td>{booking.time}:00</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">Service</th>
-                              <td>:</td>
-                              <td>{booking.service.name}</td>
-                            </tr>
-                            <tr>
-                              <th scope="row">Harga</th>
-                              <td>:</td>
-                              <td>
-                                {new Intl.NumberFormat("id-ID", {
-                                  style: "currency",
-                                  currency: "IDR",
-                                }).format(booking.service.price)}
-                                
-                              </td>
-                            </tr>
-                            <tr>
-                              <th scope="row">Detail</th>
-                              <td>:</td>
-                              <td>{booking.service.detail}</td>
-                            </tr>
-                          </tbody>
-                          <div className="d-contents mt-3">
-                            <button className="btn btn-danger">
-                              Cancel Booking
-                            </button>
-                          </div>
-                        </table>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              } else {
-                return null;
-              }
-            })}
+            {data
+              .filter((booking) => userData.id === booking.userId)
+              .sort((a, b) => {
+                const dateComparison =
+                  new Date(formatDate(a.date)) - new Date(formatDate(b.date));
+
+                if (dateComparison === 0) {
+                  // If the dates are the same, compare the times
+                  return new Date(a.time) - new Date(b.time);
+                }
+
+                return dateComparison;
+              })
+              .map((booking) => (
+                <Col key={booking.id}>
+                  <Card data-aos="fade-up" className="shadow">
+                    <Card.Body>
+                      <table className="tables">
+                        <thead className="title">Bukti Booking</thead>
+                        <tbody className="information">
+                          <tr>
+                            <th scope="row">Nama</th>
+                            <td>:</td>
+                            <td>{booking.user.username}</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Tanggal</th>
+                            <td>:</td>
+                            <td>{formatDate(booking.date)}</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Jam</th>
+                            <td>:</td>
+                            <td>{booking.time}:00</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Service</th>
+                            <td>:</td>
+                            <td>{booking.service.name}</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Harga</th>
+                            <td>:</td>
+                            <td>
+                              {new Intl.NumberFormat("id-ID", {
+                                style: "currency",
+                                currency: "IDR",
+                              }).format(booking.service.price)}
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">Detail</th>
+                            <td>:</td>
+                            <td>{booking.service.detail}</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                      <div className="d-contents mt-3">
+                        <button
+                          className="btn btn-danger"
+                          onClick={() => handleCancelBooking(booking.id)}
+                        >
+                          Cancel Booking
+                        </button>
+                      </div>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
           </Row>
         )}
       </Container>
+      <ToastContainer />
     </div>
   );
 };
