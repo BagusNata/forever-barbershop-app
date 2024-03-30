683adb112405dd2e97126dcbbc9e27a8e7c11bb8
BagusNata <bagusnata330@gmail.com>
2024-03-30 15:54:21 +0800

    create admin dashboart to manage service

---
+++ barber-user/src/Admin/components/ServicesContent.jsx
@@ -0,0 +1,160 @@
+import { Container, Row, Card } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import "../assets/ServicesContent.css";
+import Swal from "sweetalert2";
+import { useUserContext } from "../../UserContext";
+
+const ServicesContent = () => {
+  const [data, setData] = useState([]);
+  const { userData } = useUserContext();
+
+  useEffect(() => {
+    const getService = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/services`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setData(await response.json());
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    //kalau accesToken sudah ada baru di jalankan
+    if (userData.accessToken) {
+      getService();
+    }
+  }, [userData]);
+
+  const handleDeleteService = async (serviceId) => {
+    try {
+      await fetch(`${import.meta.env.VITE_API_URL}/api/services/${serviceId}`, {
+        method: "DELETE",
+        headers: {
+          "x-access-token": userData.accessToken,
+          "Content-Type": "application/json",
+        },
+      });
+
+      // After successful deletion, update the data state to re-render without the deleted service
+      setData((prevData) => prevData.filter((service) => service.id));
+
+      // Show a success notification
+      Swal.fire({
+        icon: "success",
+        title: "Service deleted successfully!",
+        text: "Service that you delete will be removed from the list.",
+        timer: 3000, // 3 seconds
+      });
+    } catch (error) {
+      console.error("Error deleting service:", error);
+      // Show an error notification
+      Swal.fire({
+        icon: "error",
+        title: "Error deleting service",
+        text: "Please try again later.",
+      });
+    }
+  };
+
+  return (
+    <div className="w-100 min-vh-100 content-body">
+      <Container>
+        <Row className="py-5">
+          <Card>
+            <div className="card-total-service d-flex justify-content-evenly">
+              <div className="d-grid">
+                <span className="fs-4">{data.length}</span>
+                <span>Services</span>
+              </div>
+              {/* Icon */}
+              <i className="fas fa-scissors icon fs-3" />
+            </div>
+          </Card>
+        </Row>
+        <Row>
+          <div className="table-responsive">
+            <table className="table table-striped table-hover table-bordered">
+              <thead className="table-dark">
+                <tr className="text-center">
+                  <th scope="col">Id</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Name</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Detail</th>
+                  <th scope="col">CreatedAt</th>
+                  <th scope="col">UpdatedAt</th>
+                  <th scope="col" colSpan={2}>
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.length === 0 ? (
+                  <tr>
+                    <td colSpan="10">
+                      <div className="d-flex align-items-center">
+                        <strong>Loading...</strong>
+                        <div
+                          className="spinner-border ms-auto"
+                          role="status"
+                          aria-hidden="true"
+                        ></div>
+                      </div>
+                    </td>
+                  </tr>
+                ) : (
+                  data.map((data) => (
+                    <tr key={data.id}>
+                      <th className="text-center">{data.id}</th>
+                      <td>
+                        <img
+                          src={data.image}
+                          alt={data.name}
+                          width={150}
+                          className="px-2"
+                        />
+                      </td>
+                      <td>{data.name}</td>
+                      <td>
+                        {new Intl.NumberFormat("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                        }).format(data.price)}
+                      </td>
+                      <td>{data.description}</td>
+                      <td>{data.detail}</td>
+                      <td>{data.createdAt}</td>
+                      <td>{data.updatedAt}</td>
+                      <td className="text-center">
+                        <a href="">
+                          <i className="fas fa-edit fs-6" />
+                          <p>Edit</p>
+                        </a>
+                      </td>
+                      <td className="text-center">
+                        <a href="" onClick={() => handleDeleteService(data.id)}>
+                          <i className="fa-solid fa-trash-can fs-6" />
+                          <p>Delete</p>
+                        </a>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default ServicesContent;
