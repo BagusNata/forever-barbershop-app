4de6c57086b1dc13d5f14341fa6f4843c3710ddc
BagusNata <bagusnata330@gmail.com>
2024-03-30 11:38:29 +0800

    add route for admin page

--- barber-user/src/App.jsx
+++ barber-user/src/App.jsx
@@ -1,39 +1,53 @@
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
 //Import Pages
 //Admin
-import AdminPage from "../src/Admin/pages/AdminPage";
+import DashboardPage from "../src/Admin/pages/dashboard/DashboardPage";
+import AdminBookingsPage from "../src/Admin/pages/dashboard/AdminBookingsPage";
+import AdminSessionPage from "../src/Admin/pages/dashboard/AdminSessionPage";
+import AdminUsersPage from "../src/Admin/pages/dashboard/AdminUsersPage";
+import AdminCapstersPage from "../src/Admin/pages/dashboard/AdminCapstersPage";
+import AdminServicesPage from "../src/Admin/pages/dashboard/AdminServicesPage";
+import AdminTestimoniesPage from "../src/Admin/pages/dashboard/AdminTestimoniesPage";
 
 //User
 import SignInPage from "../src/User/pages/SignInPage";
 import SignUpPage from "../src/User/pages/SignUpPage";
+import SignOutPage from "./User/pages/SignOutPage";
 import HomePage from "../src/User/pages/HomePage";
 import BookingPage from "../src/User/pages/BookingPage";
 import MyBookingPage from "../src/User/pages/MyBookingPage";
 import TestimonialPage from "../src/User/pages/TestimonialPage";
 import NotFoundPage from "../src/User/pages/NotFoundPage";
-import { useUserContext } from './UserContext'
+import { useUserContext } from "./UserContext";
 
 function App() {
-  const { setUserData } = useUserContext()
+  const { setUserData } = useUserContext();
 
   useEffect(() => {
-    const userStorage = localStorage.getItem('userData')
+    const userStorage = localStorage.getItem("userData");
     if (userStorage) {
-      setUserData(JSON.parse(userStorage))
+      setUserData(JSON.parse(userStorage));
     }
-  }, [])
+  }, []);
 
   return (
     <div>
       <Routes>
         {/* Admin */}
-        <Route path="/admin" Component={AdminPage} />
+        <Route path="/admin/dashboard" Component={DashboardPage} />
+        <Route path="/admin/bookings" Component={AdminBookingsPage} />
+        <Route path="/admin/session" Component={AdminSessionPage} />
+        <Route path="/admin/users" Component={AdminUsersPage} />
+        <Route path="/admin/capsters" Component={AdminCapstersPage} />
+        <Route path="/admin/services" Component={AdminServicesPage} />
+        <Route path="/admin/testimonies" Component={AdminTestimoniesPage} />
 
         {/* User */}
         <Route path="/signin" Component={SignInPage} />
         <Route path="/signup" Component={SignUpPage} />
+        <Route path="/signout" Component={SignOutPage} />
         <Route path="/" Component={HomePage} />
         <Route path="/booking" Component={BookingPage} />
         <Route path="/myBooking" Component={MyBookingPage} />
