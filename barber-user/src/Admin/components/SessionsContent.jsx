136524f4b36a9ee58383f64c77f38ae1d5ab0ce4
vedanta-tabist <bagusnata330@gmail.com>
2024-04-01 18:53:53 +0800

    create session content

---
+++ barber-user/src/Admin/components/SessionsContent.jsx
@@ -0,0 +1,149 @@
+import { Container, Row, Card } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+import "../assets/AdminContent.css";
+import Swal from "sweetalert2";
+import { useUserContext } from "../../UserContext";
+
+const SessionsContent = () => {
+  const [data, setData] = useState([]);
+  const { userData } = useUserContext();
+
+  useEffect(() => {
+    const getSessio = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/api/sessions`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        setData(await response.json());
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    //kalau accesToken sudah ada baru di jalankan
+    if (userData.accessToken) {
+      getSessio();
+    }
+  }, [userData]);
+
+  const handleDeleteSession = async (sessionId) => {
+    try {
+      await fetch(`${import.meta.env.VITE_API_URL}/api/sessions/${sessionId}`, {
+        method: "DELETE",
+        headers: {
+          "x-access-token": userData.accessToken,
+          "Content-Type": "application/json",
+        },
+      });
+
+      // After successful deletion, update the data state to re-render without the deleted session
+      setData((prevData) => prevData.filter((session) => session.id));
+
+      // Show a success notification
+      Swal.fire({
+        icon: "success",
+        title: "Session deleted successfully!",
+        text: "Session that you delete will be removed from the list.",
+        timer: 3000, // 3 seconds
+      });
+    } catch (error) {
+      console.error("Error deleting session:", error);
+      // Show an error notification
+      Swal.fire({
+        icon: "error",
+        title: "Error deleting session",
+        text: "Please try again later.",
+      });
+    }
+  };
+
+  function formatDate(dateString) {
+    return format(new Date(dateString), "PPpp");
+  }
+
+  return (
+    <div className="w-100 min-vh-100 content-body">
+      <Container>
+        <Row className="py-5">
+          <Card>
+            <div className="card-total-admin d-flex justify-content-evenly">
+              <div className="d-grid">
+                <span className="fs-4">{data.length}</span>
+                <span>Session</span>
+              </div>
+              {/* Icon */}
+              <i className="fas fa-clock icon fs-3" />
+            </div>
+          </Card>
+        </Row>
+        <Row>
+          <div className="table-responsive">
+            <table className="table table-striped table-hover table-bordered">
+              <thead className="table-dark">
+                <tr className="text-center">
+                  <th scope="col">Id</th>
+                  <th scope="col">Jam</th>
+                  <th scope="col">CreatedAt</th>
+                  <th scope="col">UpdatedAt</th>
+                  <th scope="col" colSpan={2}>
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.length === 0 ? (
+                  <tr>
+                    <td colSpan="10">
+                      <div className="d-flex align-items-center">
+                        <strong>Loading...</strong>
+                        <div
+                          className="spinner-border ms-auto"
+                          role="status"
+                          aria-hidden="true"
+                        ></div>
+                      </div>
+                    </td>
+                  </tr>
+                ) : (
+                  data.map((data) => (
+                    <tr key={data.id}>
+                      <th className="text-center">{data.id}</th>
+                      <td>{data.time}:00</td>
+                      <td>
+                        {data.createdAt ? formatDate(data.createdAt) : ""}
+                      </td>
+                      <td>
+                        {data.updatedAt ? formatDate(data.updatedAt) : ""}
+                      </td>
+                      <td className="text-center">
+                        <a href="">
+                          <i className="fas fa-edit fs-6" />
+                          <p>Edit</p>
+                        </a>
+                      </td>
+                      <td className="text-center">
+                        <a href="" onClick={() => handleDeleteSession(data.id)}>
+                          <i className="fa-solid fa-trash-can fs-6" />
+                          <p>Delete</p>
+                        </a>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default SessionsContent;
