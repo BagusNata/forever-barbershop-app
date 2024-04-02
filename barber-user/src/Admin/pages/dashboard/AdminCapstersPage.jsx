8ae5cbf49a4fe83f8888e32eeb718facdcb71859
vedanta-tabist <bagusnata330@gmail.com>
2024-04-02 11:07:47 +0800

    Add logic so that only users who have the admin role can access the page

--- barber-user/src/Admin/pages/dashboard/AdminCapstersPage.jsx
+++ barber-user/src/Admin/pages/dashboard/AdminCapstersPage.jsx
@@ -1,13 +1,39 @@
+import { useEffect } from "react";
 import Sidebar from "../../components/Sidebar";
 import CapstersContent from "../../components/CapstersContent";
+import { useUserContext } from "../../../UserContext";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const AdminCapstersPage = () => {
-  return (
+  const navigate = useNavigate();
+  const { userData } = useUserContext();
+
+  useEffect(() => {
+    // Check if user role = admin
+    if (userData && userData.roles && userData.roles.includes("ROLE_ADMIN")) {
+      // Render admin page
+      return;
+    } else {
+      // Show restricted message
+      Swal.fire({
+        icon: "error",
+        title: "Restricted to access admin page",
+        text: `Your account doesn't have admin role`,
+      }).then(() => {
+        // Redirect to the home page
+        navigate("/");
+      });
+    }
+  }, [userData, navigate]);
+
+  // Render admin page only if user has admin role
+  return userData && userData.roles && userData.roles.includes("ROLE_ADMIN") ? (
     <div className="d-flex">
       <Sidebar />
       <CapstersContent />
     </div>
-  );
+  ) : null;
 };
 
 export default AdminCapstersPage;
