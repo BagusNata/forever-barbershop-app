f07b197c5a9ef338908bf4f5f3927d758c888967
vedanta-tabist <bagusnata330@gmail.com>
2024-04-05 23:43:07 +0800

    update path for getBookingTime

--- barber-be/app/routes/booking.routes.js
+++ barber-be/app/routes/booking.routes.js
@@ -1,48 +1,36 @@
-const { authJwt } = require('../middleware');
-const controller = require('../controllers/booking.controller');
+const { authJwt } = require("../middleware");
+const controller = require("../controllers/booking.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
-      'Access-Control-Allow-Headers',
-      'x-access-token, Origin, Content-Type, Accept'
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
 
-  app.post(
-    '/api/bookings',
-    [authJwt.verifyToken],
-    controller.addBooking
-  )
+  app.post("/api/bookings", [authJwt.verifyToken], controller.addBooking);
 
   app.post(
-    '/api/bookings/:id/done',
+    "/api/bookings/:id/done",
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.doneBooking
-  )
+  );
 
   app.get(
-    '/api/bookings',
+    "/api/bookings",
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.getBookings
-  )
+  );
 
-  app.get(
-    '/api/bookings/me',
-    [authJwt.verifyToken],
-    controller.getMyBookings
-  )
+  app.get("/api/bookings/me", [authJwt.verifyToken], controller.getMyBookings);
 
-  app.post(
-    '/api/bookings/me/time',
-    [authJwt.verifyToken],
-    controller.getMyBookingsTime
-  )
+  app.post("/api/bookings/time", controller.getBookingsTime);
 
   app.delete(
-    '/api/bookings/:id',
+    "/api/bookings/:id",
     [authJwt.verifyToken],
     controller.deleteBooking
-  )
+  );
 };
