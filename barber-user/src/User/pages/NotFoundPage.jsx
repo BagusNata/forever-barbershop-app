e1d9e05ab62fd0d17842c79b7769d1ce6ab42d01
vedanta-tabist <bagusnata330@gmail.com>
2024-04-07 12:04:40 +0800

    create not found page content

--- barber-user/src/User/pages/NotFoundPage.jsx
+++ barber-user/src/User/pages/NotFoundPage.jsx
@@ -1,7 +1,27 @@
-const NotFoundPage = () => {
-  return (
-    <div>404 NOT FOUND</div>
-  )
-}
+import "../assets/css/notFoundPage.css";
+import { useNavigate } from "react-router-dom";
 
-export default NotFoundPage
+const NotFoundPage = () => {
+  const navigate = useNavigate();
+
+  // function to direct users to the home page
+  const handleClickAdd = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="not-found-page w-100 min-vh-100">
+      <div className="not-found-content position-absolute top-50 start-50 translate-middle">
+        <h1>404 |</h1> <h3>This page could not be found.</h3>
+      </div>
+      <button
+        className="btn btn-light btn-lg rounded-2 position-absolute top-50 start-50 translate-middle"
+        onClick={handleClickAdd}
+      >
+        Back to Home page
+      </button>
+    </div>
+  );
+};
+
+export default NotFoundPage;
