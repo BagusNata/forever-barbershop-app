bcd218e58dad1ef65b48b64af9f39b1f47e8aede
vedanta-tabist <bagusnata330@gmail.com>
2024-04-13 23:47:40 +0800

    test

--- barber-be/app/config/db.config.js
+++ barber-be/app/config/db.config.js
@@ -3,11 +3,12 @@ module.exports = {
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
-  dialect: 'mysql',
+  dialect: "mysql",
+  dialectModule: require("mysql2"),
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
